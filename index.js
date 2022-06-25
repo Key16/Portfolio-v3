0187d0c54f92cde26e8fc242c150f1cc39a0384b
Key <jh.yield@gmail.com>
2022-06-26 09:59:21 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// Open and close sidebar
+function openNav() {
+    document.getElementById("access-sidebar").style.width = "60%";
+    document.getElementById("access-sidebar").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("access-sidebar").style.display = "none";
+}
